d251bbb82d133cd330ded35b86d4123a0981154c
Appu Panneer Selvam <Smith.AppuPanneerSelvam@allstream.com>
2019-05-31 13:11:47 -0400

    Detail page complete with accordion title shade theme pending

--- lwc/oppOrderList/oppOrderList.js
+++ lwc/oppOrderList/oppOrderList.js
@@ -1,9 +1,24 @@
 import { LightningElement, track, api, wire } from 'lwc';
 import getOrderList from '@salesforce/apex/lgt_DataFactory.getOrderNumber';
+
 export default class OppOrderList extends LightningElement {
-@api opptyid;
-@track orderList;
-@track hasrecords = false;
-@wire(getOrderList, { orderID: '$opptyid' }) wiredwsirolist;
+    @api orderid;
+    @track orderlist;
+    @track hasrecords = false;
+    @track error;
+    @wire(getOrderList, { opportunityId: '$orderid' }) wiredOrderList({ error, data }) {
+        if (data) {
+            // alert('data=>'+data);
+            this.orderlist = data;
+            this.error = undefined;
+            if (data !== undefined && data.length > 0) {
+                this.hasrecords = true;
+            }
+        } else if (error) {
+            alert('error=>' + error);
+            this.error = error;
+            this.orderlist = undefined;
+        }
+    }
 
 }
--- lwc/opportunityRetailViewForm/opportunityRetailViewForm.js
+++ lwc/opportunityRetailViewForm/opportunityRetailViewForm.js
@@ -1,10 +1,12 @@
-import { LightningElement,api, wire } from 'lwc';
+import { LightningElement,api, wire,track } from 'lwc';
 import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 import OPPORTUNITY_ORDERID from '@salesforce/schema/Opportunity.Order__c';
 //import { getRecord } from 'lightning/uiRecordApi';
 const fields = [OPPORTUNITY_ORDERID];
+const activeOnes = ['Opp_Metrics','oppinfo','oppactit','addinfo','erinfo','spopse','loopinfo','ordst','saowwois','sysinfo'];
 export default class OpportunityRetailViewForm extends LightningElement {
     @api recordId;
+    @track activeSections = activeOnes;
     @wire(getRecord, { recordId: '$recordId', fields }) orderrecord;
     get orderNumberId(){
         return getFieldValue(this.orderrecord.data,OPPORTUNITY_ORDERID);
